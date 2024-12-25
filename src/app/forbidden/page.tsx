447ccee4e7547b24ac497614ca07bc1d44a732a3
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2024-12-25 13:24:29 +0530

    Setup Client

--- src/app/forbidden/page.tsx
+++ src/app/forbidden/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import HeroIcon from "@/components/HeroIcon/HeroIcon";
 
 import { Metadata } from "next";
@@ -12,24 +13,27 @@ export const metadata: Metadata = {
 
 export default function Forbidden() {
   return (
-    <div className="bg-gradient-to-br from-green-50 to-green-100 flex items-center justify-evenly min-h-screen space-y-4 w-full">
-      <div className="space-y-4">
-        <h3 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight">
-          Unauthorized Access
-        </h3>
-        <p className="text-lg text-gray-600">
-          You do not have permission to access this page.
-        </p>
-        <Link
-          href={"/"}
-          className="inline-flex text-center px-8 py-3 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors duration-300"
-        >
-          Go Back
-        </Link>
+    <>
+      <div className="bg-gradient-to-br from-green-50 to-green-100 flex items-center justify-evenly min-h-screen space-y-4 w-full">
+        <div className="space-y-4">
+          <h3 className="text-4xl md:text-5xl font-bold text-gray-900 leading-tight">
+            Unauthorized Access
+          </h3>
+          <p className="text-lg text-gray-600">
+            You do not have permission to access this page.
+          </p>
+          <Link
+            href={"/"}
+            className="inline-flex text-center px-8 py-3 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors duration-300"
+          >
+            Go Back
+          </Link>
+        </div>
+        <div className="mb-4 w-1/2 p-20">
+          <HeroIcon iconSrc="/forbidden.svg" height={250} width={250} />
+        </div>
       </div>
-      <div className="mb-4 w-1/2 p-20">
-        <HeroIcon iconSrc="/forbidden.svg" height={250} width={250} />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
