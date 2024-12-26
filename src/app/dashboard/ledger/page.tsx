afcacd25d542d73c638ac39713a1abfb89050717
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2024-12-27 01:39:08 +0530

    Deploying Front-end

--- src/app/dashboard/ledger/page.tsx
+++ src/app/dashboard/ledger/page.tsx
@@ -1,39 +1,32 @@
-// import {
-//   Transaction,
-//   TransactionsTable,
-// } from "@/components/TransactionTable/TransactionTable";
-// import { Metadata } from "next";
+import {
+  Transaction,
+  TransactionsTable,
+} from "@/components/TransactionTable/TransactionTable";
+import { Metadata } from "next";
 
-// const fetchLedger = async (): Promise<Array<Transaction>> => {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/ledgers`);
-//   if (!res.ok) {
-//     const errorText = await res.text();
-//     throw new Error(`Failed to fetch data: ${res.status} - ${errorText}`);
-//   }
-//   return res.json();
-// };
+const fetchLedger = async (): Promise<Array<Transaction>> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/ledgers`);
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(`Failed to fetch data: ${res.status} - ${errorText}`);
+  }
+  return res.json();
+};
 
-// export const metadata: Metadata = {
-//   title: "Ledger | WealthSetu - Your Financial Journey Starts Here",
-//   description: "WealthSetu - Your Financial Journey Starts Here",
-// };
+export const metadata: Metadata = {
+  title: "Ledger | WealthSetu - Your Financial Journey Starts Here",
+  description: "WealthSetu - Your Financial Journey Starts Here",
+};
 
-// export default async function Ledger() {
-//   const ledger = await fetchLedger();
+export default async function Ledger() {
+  const ledger = await fetchLedger();
 
-//   return (
-//     <div className="p-4 pt-0">
-//       <h1 className="my-2 text-3xl font-semibold leading-none tracking-tight">
-//         Ledger
-//       </h1>
-//       <TransactionsTable transactions={ledger} />
-//     </div>
-//   );
-// }
-import React from 'react'
-
-export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="p-4 pt-0">
+      <h1 className="my-2 text-3xl font-semibold leading-none tracking-tight">
+        Ledger
+      </h1>
+      <TransactionsTable transactions={ledger} />
+    </div>
+  );
 }
